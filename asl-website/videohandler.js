53a74d95cccfd0843858c6919b08894f8c85a7e8
Chaelin <53431568+chaelin0722@users.noreply.github.com>
2025-10-23 21:11:23 -0500

    Delete asl-website directory

--- asl-website/videohandler.js
+++
@@ -1,7 +0,0 @@
-//import { saveUserProgress } from './progress';
-//import { auth } from './auth';
-
-//export function handleVideoClick(term) {
- //   const userId = auth.currentUser.uid; // Get the current user's ID
-   // saveUserProgress(userId, term, true); // Mark the video as watched
-//}
